1e65822e9f300541fcd85f02e8b2211b55dd530f
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-02 20:40:39 +0300

    update input component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './app.scss';
+import { MarkdownInput } from './components/markdown-input/MarkdownInput';
 
 function App() {
-  return <div className="App">Learn React</div>;
+  const [appInput, setAppInput] = React.useState();
+
+  const setAppCompInput = (input) => {
+    setAppInput(input);
+    console.log(input);
+  };
+
+  return (
+    <div class="app">
+      <h1 class="app-header">Markdown Previewer</h1>
+      <div class="md-io-wrapper">
+        <MarkdownInput
+          setAppCompInput={setAppCompInput}
+          getAppInput={appInput}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
